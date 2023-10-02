7584fdf67e8c3b5543ba74088179e1c9a896ce47
Ramil-Karipov <omil8@yandex.ru>
2023-10-03 00:05:01 +0500

    Task number three

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,47 @@ public class Main {
         System.out.println("За сутки машина произвела " + day + " штук бутылок");
         System.out.println("За три дня машина произвела " + day3 + " штук бутылок");
         System.out.println("За месяц машина произвела " + month + " штук бутылок");
+        System.out.println();
+        byte jar = 120;
+        byte white = 2;
+        byte brown = 4;
+        int classRoom = 120 / (white + brown);
+        int whiteJar = classRoom * white;
+        int brownJar = classRoom * brown;
+        System.out.println("В школе, где " + classRoom + " классов, нужно " + whiteJar + " банок белой краски и " + brownJar + " банок коричневой краски");
+        System.out.println();
+        int bananas = 80 * 5;
+        int milk1 = 105 * 2;
+        int iceCream = 100 * 2;
+        int eggs = 70 * 4;
+        int totalWeight = bananas + milk1 + iceCream + eggs;
+        float weightKg = totalWeight / 1000;
+        System.out.println("Вес такого спортзавтрака " + totalWeight + " или " + weightKg + "кг");
+        System.out.println();
+        int sportsmanWeight = 7 * 1000;
+        int day1 = sportsmanWeight / 500;
+        int day2 = sportsmanWeight / 250;
+        System.out.println("Спортсмену потребуется " + day1 + " дней если, спортсмен будет терять каждый день по 500 грамм или " + day2 + " дней, если каждый день он будет худеть на 250 грамм.");
+        System.out.println();
+        int Masha = 67760;
+        int Denis = 83690;
+        int Kristina = 76230;
+        double MashaNew = Masha * 0.1 + Masha;
+        double DenisNew = Denis * 0.1 + Denis;
+        double KristinaNew = Kristina * 0.1 + Kristina;
+        double MashaYear = (MashaNew) * 12 - (Masha * 12);
+        double DenisYear = (DenisNew * 12) - (Denis * 12);
+        double KristinaYear = (KristinaNew * 12) - (Kristina * 12);
+        System.out.println("Маша теперь получает " + MashaNew + " рублей. Годовой доход вырос на " + MashaYear + " рублей");
+        System.out.println("Денис теперь получает " + DenisNew + " рублей. Годовой доход вырос на " + DenisYear + " рублей");
+        System.out.println("Кристина теперь получает " + KristinaNew + " рублей. Годовой доход вырос на " + KristinaYear + " рублей");
+
+
+
+
+
+
+
 
 
     }
